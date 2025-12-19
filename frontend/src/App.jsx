d0e4a4142011505bf157a2b95d1c4607394719f0
BrunoFujisao <brunofujisao2018@gmail.com>
2025-12-19 20:12:18 -0300

    criação do footer e botaoVerde components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import BotaoVerde from "./components/botaoVerde";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleLogin() {
+    alert("Login clicado");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ maxWidth: "300px", margin: "100px auto" }}>
+      <BotaoVerde
+        mensagem="Fazer Login"
+        onClick={handleLogin}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
