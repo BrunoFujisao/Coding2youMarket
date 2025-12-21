4e9d1b4742583add32365e63a39b123aaac9eece
Kleber Grandolffi <kleberjgrandolffi@gmail.com>
2025-12-21 00:13:29 -0300

    fix: Comentar transporter.verify temporariamente

--- BACK-END/src/Services/emailService.js
+++ BACK-END/src/Services/emailService.js
@@ -11,13 +11,13 @@ const transporter = nodemailer.createTransport({
 });
 
 // Teste de conexão
-transporter.verify((error, success) => {
+/*transporter.verify((error, success) => {
   if (error) {
     console.error("❌ Erro na conexão: Verifique se o caminho do .env está correto.");
   } else {
     console.log("✅ Conexão estabelecida com sucesso usando .env!");
   }
-});
+});*/
 
 const enviarEmailCodigo = async (emailDestino, codigo) => {
   const mailOptions = {
